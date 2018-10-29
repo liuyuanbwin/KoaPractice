33375231de7a0b34da6a030686e7500335216fa0
LeoLau <liuyuansix@163.com>
2018-10-29 14:41:55 +0800

    next 级联响应

--- app.js
+++ app.js
@@ -3,7 +3,24 @@
  */
 const Koa = require('koa');
 const app = new Koa();
-app.use(async ctx => {
-  ctx.body =  "Hello World!";
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get('X-Response-Time');
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+  console.log('logger');
 });
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+  console.log('response');
+});
+
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+  console.log('hello world');
+});
+
 app.listen(3000);
